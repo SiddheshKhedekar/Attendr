d198be9d9700186c936f6380e2add3b288517bd0
SiddheshKhedekar <siddheshkhedekar.dev@gmail.com>
2021-10-23 12:15:57 +0530

    add auto redirection if organization logged in before

--- app/src/main/java/dev/sid/attendr/OrganizationActivity.java
+++ app/src/main/java/dev/sid/attendr/OrganizationActivity.java
@@ -1,9 +1,10 @@
 package dev.sid.attendr;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -16,28 +17,36 @@ public class OrganizationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPref = OrganizationActivity.this.getPreferences(Context.MODE_PRIVATE);
+        String defaultOrganization = getResources().getString(R.string.saved_default_organization);
+        String savedOrganization = sharedPref.getString(getString(R.string.saved_organization), defaultOrganization);
+        if (!savedOrganization.equals(defaultOrganization)) {
+            redirectToLogin();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization);
 
         FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
         AttendrFirestoreConnector attendrFsCon = new AttendrFirestoreConnector(mFirestore);
         EditText org = findViewById(R.id.org);
-        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
 
         Button next = findViewById(R.id.next);
         next.setOnClickListener(view -> {
             String organization = org.getText().toString();
             attendrFsCon.checkIfOrganizationExists(attendrCallback -> {
                 if (attendrCallback) {
-                    sendMessage();
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(getString(R.string.saved_organization), organization);
+                    editor.apply();
+                    redirectToLogin();
                 }
             },organization);
             Log.d(TAG, "Callback data exists");
         });
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage() {
+    /** Called when the organization exist or is created */
+    public void redirectToLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
